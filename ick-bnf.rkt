0266506d5925f6b6fd728e855bbb67522e07e92b
Ethan <ethan.hawk@valpo.edu>
2026-03-27 10:28:46 -0400

    ick-bnf.rkt ~ more work

--- ick-bnf.rkt
+++ ick-bnf.rkt
@@ -2,41 +2,86 @@
 
 program : line+
 
-line : label stmt
+line : label? stmt
 
-label : NUMBER
+label : LPAREN NUMBER RPAREN
 
-stmt : polite? op
+do-prefix : PLEASE
+          | DO
+          | NOT
+          | MAYBE
 
-polite : DO
-       | PLEASE DO
-       | PLEASE
+do-postfix : ONCE
+           | AGAIN
+
+stmt : do-prefix* op do-postfix*
 
 op : assign
    | next
    | comefrom
    | readout
    | giveup
+   | writein
+   | stash
+   | retrieve
+   | forget
+   | resume
+   | abstain
+   | reinstate
+   | nothing
 
 assign : var GETS expr
-next : NEXT NUMBER
-comefrom : COME FROM NUMBER
+
+target : NUMBER
+       | LPAREN NUMBER RPAREN
+       | MESH NUMBER 
+
+next : target NEXT
+comefrom : COME FROM target
+
 readout : READ OUT expr
 giveup : GIVE UP
 
-ident : NUMBER
-      | ID
+abstain-target : target
+
+writein : WRITE IN var
+stash : STASH expr
+retrieve : RETRIEVE expr
+forget : FORGET expr
+resume : RESUME expr
+
+abstain : ABSTAIN FROM abstain-target
+reinstate : REINSTATE abstain-target
+nothing : NOTHING
+
+ident : NUMBER | ID
 
 var : DOT ident
     | COLON ident
-    | STAR ident
+    | COMMA ident
     | var SUB expr
 
-expr : UNARY_AND expr
-     | UNARY_OR expr
-     | UNARY_XOR expr
-     | expr MINGLE expr
-     | expr SELECT expr
-     | var
-     | NUMBER
-     | MESH NUMBER
+expr : mingle
+
+mingle
+  : select
+  | mingle MINGLE select
+
+select
+  : unary
+  | select SELECT unary
+
+unary
+  : UNARY_AND unary
+  | UNARY_OR unary
+  | UNARY_XOR unary
+  | postfix
+
+postfix
+  : primary
+  | postfix SUB expr
+
+primary
+  : var
+  | NUMBER
+  | MESH NUMBER
