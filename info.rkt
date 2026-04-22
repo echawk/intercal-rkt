7ca4c734d241776685b5b56b847c818b3a081b80
Ethan <ethan.hawk@valpo.edu>
2026-04-22 15:40:25 -0400

    info.rkt : specify license as MIT

--- info.rkt
+++ info.rkt
@@ -4,6 +4,7 @@
 (define name "sick")
 (define version "0.1")
 (define pkg-desc "INTERCAL implemented in Racket with a reader, normalizer, and macro-compiled runtime.")
+(define license 'MIT)
 (define deps '("roman-numeral" "brag"))
 (define build-deps '("rackunit-lib" "scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/intercal.scrbl" (multi-page) (language))
